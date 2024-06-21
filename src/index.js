4c13fd08082b06348e96baa9762080eab05eb058
Raushan Kumar <raushankumarguptag@gmail.com>
2024-06-21 17:17:13 +0530

    utils

--- src/index.js
+++ src/index.js
@@ -1,6 +1,15 @@
+import { app } from "./app.js";
 import connectDB from "./db/db.js";
 import { config } from "dotenv";
 
 config({ path: "./env" });
 
-connectDB();
+connectDB()
+  .then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`Server is running on port ${process.env.PORT || 8000}`);
+    });
+  })
+  .catch((err) => {
+    console.error("DB CONNECTION FAILED", err);
+  });
---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,21 @@
+// saync code handler
+
+// const asyncHandler = (fun) => async (req, res, next) => {
+//   try {
+//     await fun(req, res, next);
+//   } catch (error) {
+//     res.status(error.code || error).json({
+//       success: false,
+//       message: error.message,
+//     });
+//   }
+// };
+
+// export { asyncHandler };
+
+const asyncHandler = (requestHandler) => {
+  (req, res, next) => {
+    Promise.resolve(requestHandler(req, res, next)).catch((err) => next(err));
+  };
+};
+export { asyncHandler };
